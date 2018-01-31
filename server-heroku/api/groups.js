c36e826790c86a3ac8593d30e3f6587de31e0c09
sk521 <samkwon521@gmail.com>
2018-01-31 15:50:00 -0500

    back end socket io

--- server-heroku/api/groups.js
+++ server-heroku/api/groups.js
@@ -21,4 +21,23 @@ groupRouter.post('/:id/events', async (ctx, next) => {
   ctx.body = newEvent;
 })
 
+/*
+We are updating the User's latitude and longitude when they join an event.
+Currently groups can only have one event, otherwise there will be two latitudes and longitudes
+
+STILL WORKING ON THIS!!
+*/
+groupRouter.put('/:id/events/:eventId', async (ctx, next) => {
+  let event = await ctx.db.models.event.findById(ctx.params.eventId);
+  if (event.startEvent || Number(event.groupId) !== Number(ctx.params.id)) {
+    ctx.status = 410;
+  } else {
+    let group = await ctx.db.models.group.findById(ctx.params.id);
+    ctx.body = await group.addUser(ctx.request.body.userId, {through: {
+      latitude: ctx.req.body.latitude,
+      longitude: ctx.req.body.longitude
+    }})
+  }
+})
+
 module.exports = groupRouter;
--- server-heroku/socket/index.js
+++ server-heroku/socket/index.js
@@ -1,9 +1,16 @@
+const axios = require('axios');
+
 module.exports = (io) => {
   io.on('connection', (socket) => {
     console.log(`A socket connection to the server has been made! with ${socket.id}`)
 
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the eatery!`)
+    })
+
+    socket.on('createRoom', ({ eventId, hostId, groupId, latitude, longitude }) => {
+      // Unsure about restfulness
+      axios.put(`/api/groups/${groupId}/events/${eventId}`, {userId: hostId, latitude, longitude})
     })
   });
 };

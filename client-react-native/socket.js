b321b6cee230ac4e4ec919c0fa8d3fc3074f44a7
Sarah Zhao <sarahzhao25@gmail.com>
2018-02-01 00:11:34 -0500

    refactor groupEvent PUT route and initialized restaurant connectivity

--- client-react-native/socket.js
+++ client-react-native/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 import {SERVER} from './serverInfo';
-import store, { getUsers, getRestaurants} from './store';
+import store, { getUsers, fetchRestaurants} from './store';
 
 const socket = io(SERVER)
 
@@ -9,15 +9,14 @@ socket.on('connect', () => {
 })
 
 //If there is a midpoint sent from the backend, then we need to alert ALL the users involved in the event that they can move on to the next view.
-socket.on('currentStatus', (currentGroupStatusArr) => {
-  let midpoint = currentGroupStatusArr.filter(points => points.id === 'midpoint')[0];
-  if (midpoint) {
-   // store.dispatch(getRestaurants(midpoint));
-    store.dispatch(getUsers(currentGroupStatusArr))
+socket.on('currentStatus', ({users, midpoint}) => {
+  if (midpoint.latitude) {
+    store.dispatch(fetchRestaurants(midpoint));
+    store.dispatch(getUsers(users));
    // store.dispatch ...=> send to the next view. The above is currently a placeholder.
   }
   else {
-    store.dispatch(getUsers(currentGroupStatusArr));
+    store.dispatch(getUsers(users));
   }
 })
 
--- server-heroku/socket/index.js
+++ server-heroku/socket/index.js
@@ -4,20 +4,16 @@ module.exports = (io) => {
   io.on('connection', (socket) => {
     console.log(`A socket connection to the server has been made! with ${socket.id}`)
 
-    socket.on('createRoom',
-      (data) => console.log(data)
-    )
-
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the eatery!`)
     })
 
     socket.on('createRoom', async ({ eventId, hostId, groupId, latitude, longitude }) => {
       // Unsure about restfulness
-      let currentGroupStatusArr = await axios.put(`/api/groups/${groupId}/events/${eventId}`, { userId: hostId, latitude, longitude });
-      if (currentGroupStatusArr.length) {
+      let {users, midpoint} = await axios.put(`/api/groups/${groupId}/events/${eventId}`, { userId: hostId, latitude, longitude });
+      if (users.length) {
         socket.join(`${eventId}`); //join the event with that event ID as its name
-        io.to(`${eventId}`).emit('currentStatus', currentGroupStatusArr); //send back the array
+        io.to(`${eventId}`).emit('currentStatus', {users, midpoint}); //send back the array
       }
 
     })

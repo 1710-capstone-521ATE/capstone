842c06123151cdc04e7300ad25386ab7eabbc251
Sandy Mak <sandymak91@gmail.com>
2018-02-05 17:50:18 -0500

    fixed api/groups route to remove unwanted user info

--- server-heroku/api/restaurants.js
+++ server-heroku/api/restaurants.js
@@ -1,26 +1,26 @@
-const Router = require('koa-router');
-const restRouter = new Router({ prefix: '/api/restaurants' });
-const request = require('koa-http-request');
-const app = new Router();
-const axios = require('axios');
-const { yelpKey } = require('../secrets')
+// const Router = require('koa-router');
+// const restRouter = new Router({ prefix: '/api/restaurants' });
+// const request = require('koa-http-request');
+// const app = new Router();
+// const axios = require('axios');
+// const { yelpKey } = require('../secrets')
 
 
-restRouter.get('/:lat/:long', async(ctx, next) => {
-  let lat = ctx.params.lat;
-  let long = ctx.params.long;
-  const url = `https://api.yelp.com/v3/businesses/search?latitude=${lat}2&longitude=${long}&categories=restaurants`
-  ctx.body = await axios(
-    {
-      method: 'GET',
-      url: url,
-      headers: {
-        Authorization:
-        `BEARER ${yelpKey}`
-      }
-    }
-  ).then((restaurants) => {return restaurants.data.businesses})
-  ctx.status = 200;
-})
+// restRouter.get('/:lat/:long', async(ctx, next) => {
+//   let lat = ctx.params.lat;
+//   let long = ctx.params.long;
+//   const url = `https://api.yelp.com/v3/businesses/search?latitude=${lat}&longitude=${long}&categories=restaurants`
+//   ctx.body = await axios(
+//     {
+//       method: 'GET',
+//       url: url,
+//       headers: {
+//         Authorization:
+//         `BEARER ${yelpKey}`
+//       }
+//     }
+//   ).then((restaurants) => {return restaurants.data.businesses})
+//   ctx.status = 200;
+// })
 
-module.exports = restRouter;
+// module.exports = restRouter;

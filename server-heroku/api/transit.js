2374260d019b174d0434895169e869250ca8338d
Sarah Zhao <sarahzhao25@gmail.com>
2018-02-07 15:11:19 -0500

    fixed the transit time API FE call

--- server-heroku/api/transit.js
+++ server-heroku/api/transit.js
@@ -5,17 +5,12 @@ const axios = require('axios');
 
 transitRouter.post('/', async (ctx, next) => {
     const {userLat, userLong, restLat, restLong} = ctx.request.body;
-    console.log(ctx.request.body)
     const transitResult = await axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${userLat},${userLong}&destinations=${restLat},${restLong}&transit_mode=transit&key=${googleMapTransitId}`)
 
-    const transitTime = await transitResult.data.rows[0].elements[0].duration.text
-    
-    ctx.body = transitTime;
+    const transitTime = await transitResult.data.rows[0].elements[0].duration.text;
 
-    console.log('transittime', transitTime)
-    console.log('ctxbody', ctx.body)
-
-    //I DONT KNOW WHAT NEEDS TO GO HERE AAAAAGH IT DOESN'T CARE ABOUT CTX BODY
+    const resultElements = transitResult.data.rows[0].elements[0];
+    ctx.body = resultElements;
 })
 
 
